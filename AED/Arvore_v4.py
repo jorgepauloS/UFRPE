936085d765c95a7893b682ee7e21f3e67ff38ad0
jorgepauloS <n_i_ho@hotmail.com>
2017-07-18 17:48:06 -0300

    TAEF
    Implementados:
    
    Irmão;
    É esquerdo/direito;
    Informação;
    Pai;
    Pegar esquerdo/direito;
    Adicionar;
    Buscar;
    Máximo;
    Mínimo;
    Sucessor;
    Antecessor;
    Verificar Repetidos.
    
    A implementar:
    
    Deletar;
    Balancear.

---
+++ AED/Arvore_v4.py
@@ -0,0 +1,165 @@
+class Noh:
+    def __init__(self,idNoh,listaDados):
+        self.idNoh = idNoh
+        self.listaDados = listaDados
+    def Dados(self):
+        return self.listaDados
+    def getId(self):
+        return self.idNoh
+
+class ArvoreBn:
+    def __init__(self, nNoh, nPai):
+        self.Pai=nPai
+        self.fEsquerdo=None
+        self.fDireito=None
+        self.Noh=nNoh
+        
+    def getIrmao(self,nNoh):
+        temp = self.getPai(nNoh)
+        if temp == None:
+            return None
+        temp2 = self.busca(Noh(temp,None))
+        if self.ehEsquerdo(nNoh):
+            if temp2.fDireito:
+                return temp2.fDireito.Noh.getId()
+            else:
+                return None
+        elif self.ehDireito(nNoh):
+            if temp2.fEsquerdo:
+                return temp2.fEsquerdo.Noh.getId()
+            else:
+                return None
+        else:
+            return None
+        
+    def ehEsquerdo(self,nNoh):
+        temp = self.getPai(nNoh)
+        if temp == None:
+            return False
+        temp2 = self.busca(Noh(temp,None))
+        temp3 = None
+        if temp2.fEsquerdo:
+            temp3 = temp2.fEsquerdo.Noh.getId()
+            if temp3 == nNoh.getId():
+                return True
+            else:
+                return False
+            
+    def ehDireito(self,nNoh):
+        temp = self.getPai(nNoh)
+        if temp == None:
+            return False
+        temp2 = self.busca(Noh(temp,None))
+        temp3 = None
+        if temp2.fDireito:
+            temp3 = temp2.fDireito.Noh.getId()
+            if temp3 == nNoh.getId():
+                return True
+            else:
+                return False
+
+    def getInfo(self,Noh):
+        return self.busca(Noh).Noh.Dados()
+    
+    def getPai(self,Noh):
+        temp = self.busca(Noh)
+        if temp.Pai:
+            return temp.Pai.Noh.getId()
+        else:
+            return None
+        
+    def getEsquerdo(self,Noh):
+        temp = self.busca(Noh)
+        if temp.fEsquerdo:
+            return temp.fEsquerdo
+        else:
+            return None
+        
+    def getDireito(self,Noh):
+        temp = self.busca(Noh)
+        if temp.fDireito:
+            return temp.fDireito
+        else:
+            return None
+        
+    def add(self, Noh):
+        if Noh.idNoh>=self.Noh.idNoh:
+            self.addDireito(Noh)
+        else:
+            self.addEsquerdo(Noh)
+
+    def addEsquerdo(self,Noh):
+        if self.fEsquerdo:
+            self.fEsquerdo.add(Noh)
+        else:
+            self.fEsquerdo=ArvoreBn(Noh,self)
+
+    def addDireito(self,Noh):
+        if self.fDireito:
+            self.fDireito.add(Noh)
+        else:
+            self.fDireito=ArvoreBn(Noh,self)
+
+    def busca(self,Noh):
+        if Noh.idNoh>self.Noh.idNoh:
+            if self.fDireito:
+                return self.fDireito.busca(Noh)
+            else:
+                return None
+        elif Noh.idNoh<self.Noh.idNoh:
+            if self.fEsquerdo:
+                return self.fEsquerdo.busca(Noh)
+            else:
+                return None
+        else:
+            return self
+
+    def maximo(self,nNoh):
+        temp = self.busca(nNoh)
+        if temp == None:
+            return None
+        while temp.getDireito(temp.Noh):
+            temp = temp.getDireito(temp.Noh)
+        return temp.Noh
+    
+    def minimo(self,nNoh):
+        temp = self.busca(nNoh)
+        if temp == None:
+            return None
+        while temp.getEsquerdo(temp.Noh):
+            temp = temp.getEsquerdo(temp.Noh)
+        return temp.Noh
+    
+    def sucessor(self,nNoh):
+        temp = self.busca(nNoh).getDireito(nNoh)
+        if temp:
+            return self.minimo(temp.Noh).getId()
+        else:
+            None
+            
+    def antecessor(self,nNoh):
+        temp = self.busca(nNoh).getEsquerdo(nNoh)
+        if temp:
+            return self.maximo(temp.Noh).getId()
+        else:
+            None
+    def verificar(self,nNoh):
+        buscado = self.busca(nNoh)
+        if buscado.Noh.getId() == buscado.fDireito.Noh.getId():
+            return True
+        if self.sucessor(nNoh) == nNoh.getId():
+            return True
+        else:
+            return False
+    def deletar(self,nNoh):
+        return None
+    def balancear(self):
+        return None
+
+testearv = ArvoreBn(Noh(14,[14]),None)
+lista = [15,4,9,7,18,3,5,16,4,18,17,9,14,5]
+for i in lista:
+    testearv.add(Noh(i,[i]))
+NohBusca = Noh(18,[])
+print(testearv.verificar(NohBusca))
+
